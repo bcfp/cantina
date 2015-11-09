764b88efca13d89400104749e4802f11fd0477bb
bcfp <interdisciplinar.bcfp@gmail.com>
2015-11-09 12:55:39 -0200

    Bruno

--- CantinaFgp/src/main/java/bo/LoginBO.java
+++ CantinaFgp/src/main/java/bo/LoginBO.java
@@ -1,11 +1,19 @@
 package bo;
 
+import daoimpl.LoginDAO;
+import daoservice.ILoginDAO;
 import vo.UsuarioVO;
 
 public class LoginBO {
+	
+	private ILoginDAO loginDao;
+	
+	{
+		loginDao = new LoginDAO();
+	}
 
 	public boolean isUsuarioLogado(UsuarioVO usuario) {
-		return true;
+		return loginDao.logarUsuario(usuario);
 	}
 
 }
---
+++ CantinaFgp/src/main/java/daoimpl/LoginDAO.java
@@ -0,0 +1,15 @@
+package daoimpl;
+
+import vo.UsuarioVO;
+import daoservice.ILoginDAO;
+
+public class LoginDAO implements ILoginDAO {
+
+	@Override
+	public boolean logarUsuario(UsuarioVO usuario) {
+		return true;
+	}
+
+	
+	
+}
