84f412237d0fb412d18d12fe06eabbf70deb8121
bcfp <interdisciplinar.bcfp@gmail.com>
2015-11-13 09:37:47 -0200

    Bruno

--- CantinaFgp/src/main/java/bo/ProdutoVendaBO.java
+++ CantinaFgp/src/main/java/bo/ProdutoVendaBO.java
@@ -1,8 +1,12 @@
 package bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import vo.ProdutoVendaVO;
 import daoimpl.ProdutoVendaDAO;
 import daoservice.IProdutoVendaDAO;
+import enumeradores.TipoProduto;
 
 public class ProdutoVendaBO extends ProdutoBO<ProdutoVendaVO, IProdutoVendaDAO> {
 	
@@ -15,6 +19,47 @@ public class ProdutoVendaBO extends ProdutoBO<ProdutoVendaVO, IProdutoVendaDAO> 
 	public ProdutoVendaBO() {
 		super(produtoVendaDao);
 	}
+	
+	public List<ProdutoVendaVO> filtrarProdutoFabricadoPorCodigoENome(String nome, String cod){
+		
+		List<ProdutoVendaVO> produtosVenda = filtrarProdutoVendaPorCodigoENome(nome, cod);
+		List<ProdutoVendaVO> produtosFabricados = new ArrayList<ProdutoVendaVO>();
+		
+		for (ProdutoVendaVO produtoVenda : produtosVenda) {
+			
+			if(produtoVenda.getTipo().equals(TipoProduto.PRODUCAO)){
+				
+				produtosFabricados.add(produtoVenda);
+				
+			}
+			
+		}
+		
+		return produtosFabricados;
+		
+	}
+	
+	public List<ProdutoVendaVO> filtrarProdutoRevendaPorCodigoENome(String nome, String cod){
+		
+		List<ProdutoVendaVO> produtosVenda = filtrarProdutoVendaPorCodigoENome(nome, cod);
+		List<ProdutoVendaVO> produtosRevenda = new ArrayList<ProdutoVendaVO>();
+		
+		for (ProdutoVendaVO produtoVenda : produtosVenda) {
+			
+			if(produtoVenda.getTipo().equals(TipoProduto.REVENDA)){
+				
+				produtosRevenda.add(produtoVenda);
+				
+			}
+			
+		}
+		
+		return produtosRevenda;
+		
+	}
+	
+	
+
 	
 
 }
